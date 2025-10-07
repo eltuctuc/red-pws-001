00c6ef499cc44fb350e24859437892e62f5ef267
Enrico Reinsdorf <enrico.reinsdorf@re-design.de>
2025-10-08 00:22:06 +0200

    Personenübersicht angelegt

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+  },
+  {
+    path: '/employees',
+    name: 'employees',
+    component: () => import(/* webpackChunkName: "employees" */ '../views/EmployeesView.vue'),
   },
   {
     path: '/about',
@@ -13,13 +18,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
